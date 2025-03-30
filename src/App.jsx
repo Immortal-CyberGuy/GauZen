f156a204e42322060a257fe579a150cf4a9d7738
CyberZenith <gargshubham2411@gmail.com>
2025-03-30 15:09:12 +0530

    Footer updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CowPedia from "./pages/CowPedia";
-// import ChatBot from "./pages/ChatBot";
 import BreedMatching from "./pages/BreedMatching";
 import Marketplace from "./pages/MarketPlace";
+import Footer from "./components/Footer"; 
 import Navbar from "./components/Navbar"; 
-// import Chatbot from "./components/Chatbot/Chatbot";  // Import Chatbot
 
 function App() {
   return (
-    <Router>
-      <Navbar /> {/* Include Navbar */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cow-pedia" element={<CowPedia />} />
-        <Route path="/breed-matching" element={<BreedMatching />} />
-        <Route path="/marketplace" element={<Marketplace />} />
-      </Routes>
-      {/* <Chatbot /> */}
-    </Router>
+    <div className="app-container">
+      <Router>
+        <Navbar /> 
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cow-pedia" element={<CowPedia />} />
+            <Route path="/breed-matching" element={<BreedMatching />} />
+            <Route path="/marketplace" element={<Marketplace />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
